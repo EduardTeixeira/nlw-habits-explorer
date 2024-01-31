5add6e80ea4062c05bc8f89f59af1d63e5cc25b5
Eduard Teixeira <eduardteixeira01@gmail.com>
2024-01-31 11:29:28 -0300

    aula 4: atualizando o script e salvando os dados no localStorage do navegador

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  //const today = "01/01"
+
+  if (nlwSetup.dayExists(today)) {
+    alert("Dia já incluso ❌")
+    return
+  }
+
+  alert("Dia adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
+
+/*
 const data = {
   run: ["01-01", "01-02", "01-03"],
   water: ["01-01", "01-03", "01-04"],
@@ -8,6 +34,4 @@ const data = {
   journal: ["01-01", "01-07", "01-09"],
   takePills: ["01-11", "01-16", "01-18", "01-20"],
 }
-
-nlwSetup.setData(data)
-nlwSetup.load()
+*/
